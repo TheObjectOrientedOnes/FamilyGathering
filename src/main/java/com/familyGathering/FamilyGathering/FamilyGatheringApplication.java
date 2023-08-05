f13deaab4b173d2c1d622480cf5c114c2cc16cdd
Juan C Olmedo <doubleparked88@gmail.com>
2023-08-04 18:39:05 -0700

    initial commit

---
+++ src/main/java/com/familyGathering/FamilyGathering/FamilyGatheringApplication.java
@@ -0,0 +1,13 @@
+package com.familyGathering.FamilyGathering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyGatheringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyGatheringApplication.class, args);
+	}
+
+}
