8025dce2302c4e044750b810e5350cae3d9a07ea
Juan C Olmedo <doubleparked88@gmail.com>
2023-08-08 12:06:04 -0700

    models updated with getters and setters using the correct this keyword

--- src/main/java/com/familyGathering/familyGathering/models/FamilyModel.java
+++ src/main/java/com/familyGathering/familyGathering/models/FamilyModel.java
@@ -27,5 +27,43 @@ public class FamilyModel {
         this.familyId = id;
     }
 
+    public Long getFamilyId() {
+        return this.familyId;
+    }
 
+    public void setFamilyId(Long familyId) {
+        this.familyId = familyId;
+    }
+
+    public String getFamilyName() {
+        return this.familyName;
+    }
+
+    public void setFamilyName(String familyName) {
+        this.familyName = familyName;
+    }
+
+    public int getZipcode() {
+        return this.zipcode;
+    }
+
+    public void setZipcode(int zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public List<EventModel> getFamilyEvents() {
+        return this.familyEvents;
+    }
+
+    public void setFamilyEvents(List<EventModel> familyEvents) {
+        this.familyEvents = familyEvents;
+    }
+
+    public List<FamilyMemberModel> getFamilyMembers() {
+        return this.familyMembers;
+    }
+
+    public void setFamilyMembers(List<FamilyMemberModel> familyMembers) {
+        this.familyMembers = familyMembers;
+    }
 }
